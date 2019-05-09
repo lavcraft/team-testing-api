d538dc6adeb1742aeef0aceff4ae62f1b75dfbc4
Tolkachev Kirill <lavcraft@gmail.com>
2019-05-09 22:44:27 +0300

    chore(*): step 3. Add cors

--- src/main/java/ru/team/testing/testtestingapi/OfferController.java
+++ src/main/java/ru/team/testing/testtestingapi/OfferController.java
@@ -3,6 +3,7 @@ package ru.team.testing.testtestingapi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(value = "http://demo.ru:3000", allowCredentials = "true")
 public class OfferController {
     private final OffersRepository offersRepository;
 
