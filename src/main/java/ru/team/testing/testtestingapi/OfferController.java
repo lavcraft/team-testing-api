68d0463afd584c11aa78246dbad5a433538c1a09
Tolkachev Kirill <lavcraft@gmail.com>
2019-05-16 19:29:48 +0300

    chore(*): remove cors for demo

--- src/main/java/ru/team/testing/testtestingapi/OfferController.java
+++ src/main/java/ru/team/testing/testtestingapi/OfferController.java
@@ -3,7 +3,6 @@ package ru.team.testing.testtestingapi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +10,6 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(value = "http://localhost:3000", allowCredentials = "true")
 public class OfferController {
     private final OffersRepository offersRepository;
 
